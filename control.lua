2a36df37dc69a474b7ec8a82b0cdc0dd8846d722
TheTaurunti <thetaurunti@gmail.com>
2024-12-06 18:11:42 -0800

    HYUUUUGE 1.0.4 update

--- control.lua
+++ control.lua
@@ -5,10 +5,13 @@ local _mod_usage_message_locale = {
 
 local first_inventory_check_has_happened = false
 local function no_hard_stop_filter_message(player)
-  local msg = _mod_usage_message_locale[player.locale]
-  if (msg == nil) then msg = _mod_usage_message_locale["en"] end
-  player.print(msg)
-  player.print({ "mod-name.InventoryPlaceables" })
+  if (not first_inventory_check_has_happened)
+  then
+    first_inventory_check_has_happened = true
+    local msg = _mod_usage_message_locale[player.locale]
+    if (msg == nil) then msg = _mod_usage_message_locale["en"] end
+    player.print(msg)
+  end
 end
 
 -- ===================================
@@ -27,6 +30,7 @@ local function get_two_tier_lookup_value(table, first, second)
   if (not table[first]) then return nil end
   return table[first][second]
 end
+
 
 -- Even when the quality mod is disabled, items have "normal" quality.
 -- > that is to say, it never seems to be "nil"
@@ -78,24 +82,6 @@ local function ItemCanBePlaced(item_prototype)
 end
 
 
--- Just need to compile this information once
-local _placeable_item_prototypes_sorted = nil
-local function get_placeable_item_prototypes_sorted()
-  if (_placeable_item_prototypes_sorted == nil)
-  then
-    _placeable_item_prototypes_sorted = {}
-    -- k == name, v == prototype
-    for k, v in pairs(prototypes.item) do
-      if (ItemCanBePlaced(v))
-      then
-        table.insert(_placeable_item_prototypes_sorted, v)
-      end
-    end
-  end
-
-  return _placeable_item_prototypes_sorted
-end
-
 local function get_hard_stop_index(inventory)
   -- Fish or Deco Planner indicates a hard stop point.
   -- >> This is to preserve the player-defined filters
@@ -140,14 +126,9 @@ script.on_event(defines.events.on_gui_opened, function(event)
   local hard_stop_index = get_hard_stop_index(inventory)
   if (not hard_stop_index)
   then
-    if (not first_inventory_check_has_happened)
-    then
-      first_inventory_check_has_happened = true
-      no_hard_stop_filter_message(player)
-    end
+    no_hard_stop_filter_message(player)
     return
   end
-
 
   -- Block player-set inventory filters
   -- > same steps as "set_quickbar_cache()"
@@ -165,12 +146,13 @@ script.on_event(defines.events.on_gui_opened, function(event)
 
   -- Get lists of things to not make filters for
   local local_quickbar_cache = get_quickbar_cache(player)
-
-  -- put together a quick reference table
-  local inventory_items_qual_data = {}
+  local inventory_items_qual_stacks = {}
 
   -- inventory contents here is an array of https://lua-api.factorio.com/2.0.24/concepts/ItemWithQualityCounts.html
   local inventory_contents = inventory.get_contents()
+  local PLAYER_MAX_FILTERS_PER_ITEM = player.mod_settings["InventoryPlaceables-max-slots-filtered-per-item"].value
+  local total_filters_to_set = 0
+
   for _, item in ipairs(inventory_contents) do
     local name = item.name
     local quality = get_quality(item)
@@ -184,59 +166,18 @@ script.on_event(defines.events.on_gui_opened, function(event)
 
     if (not item_is_excluded)
     then
+      local stacks = math.min(PLAYER_MAX_FILTERS_PER_ITEM, math.ceil(item.count / item_prototype.stack_size))
+
       add_to_two_tier_lookup(
-        inventory_items_qual_data,
+        inventory_items_qual_stacks,
         name,
         quality,
-        {
-          stacks = math.ceil(item.count / item_prototype.stack_size),
-
-          -- these two used while swapping items
-          start_index = nil,
-          filters_set = 0
-        }
+        stacks
       )
+
+      total_filters_to_set = total_filters_to_set + stacks
     end
   end
-
-  -- local lua_inventory_content_example = {
-  --   -- type: ItemCountWithQuality
-  --   name = "",
-  --   count = 0,   -- uint
-  --   quality = {} -- QualityID, which is either a string or a:
-  --   -- https://lua-api.factorio.com/latest/classes/LuaQualityPrototype.html
-  -- }
-
-
-  -- By iterating over the sorted list, items should be set in
-  -- ... the placeable table in the correct order.
-  local placeable_item_prototypes = get_placeable_item_prototypes_sorted()
-  local PLAYER_MAX_FILTERS_PER_ITEM = player.mod_settings["InventoryPlaceables-max-slots-filtered-per-item"].value
-
-  -- Final step uses this: highlights placeables using filters.
-  local total_filters_to_set = 0
-  local next_start_filter_index = 1
-
-  -- The outer loop (placeable prototypes) is just to maintain the default sorting order that factorio uses.
-  for _, item in ipairs(placeable_item_prototypes) do
-    -- Only working with items that are actually in the inventory
-    if (inventory_items_qual_data[item.name])
-    then
-      -- add data for each quality level permutation of the item
-      for quality, data in pairs(inventory_items_qual_data[item.name]) do
-        data.start_index = next_start_filter_index
-
-        local slots_needed = math.min(PLAYER_MAX_FILTERS_PER_ITEM, data.stacks)
-
-        next_start_filter_index = next_start_filter_index + slots_needed
-        total_filters_to_set = total_filters_to_set + slots_needed
-      end
-    end
-  end
-
-  -- ================================   -- ================================
-  -- HERE: Time to smartify things. Move items BEFORE setting fitlers.
-  -- ================================   -- ================================
 
   -- Return to normal inventory state when there's too many things to highlight.
   local inventory_has_space_for_sorting = total_filters_to_set < hard_stop_index
@@ -245,49 +186,57 @@ script.on_event(defines.events.on_gui_opened, function(event)
     -- Setting this to 0 will clear all mod-area filters in following loop.
     total_filters_to_set = 0
   else
-    -- Iterate through inventory slots, moving items / setting filters as needed.
-    -- > Don't need special logic to exit early or exclude slots. Cases such as that...
-    -- ... are handled implicitly based on data collected thus far.
-    local inventory_slots_already_swapped_into = {}
+    -- data structure setup for recording item positions
+    local item_qual_locations = {}
+    for name, qual_stacks in pairs(inventory_items_qual_stacks) do
+      item_qual_locations[name] = {}
+      for qual, _ in pairs(qual_stacks) do
+        item_qual_locations[name][qual] = {}
+      end
+    end
     for i = 1, #inventory do
-      local swapped_here = inventory_slots_already_swapped_into[i]
-      local read_valid = inventory[i].valid_for_read
-      if (not swapped_here and read_valid)
+      if (inventory[i].valid_for_read)
       then
-        -- https://lua-api.factorio.com/2.0.24/classes/LuaInventory.html#index_operator
-        -- use field of return val: https://lua-api.factorio.com/2.0.24/classes/LuaItemStack.html
-        local item_stack = inventory[i]
-        local item_quality = get_quality(item_stack)
-
-        -- Nil value means the item doesn't need filters or swaps into correct place.
-        local swap_filter_data = get_two_tier_lookup_value(inventory_items_qual_data, item_stack.name, item_quality)
-        if (swap_filter_data)
+        local item_name = inventory[i].name
+        local item_quality = get_quality(inventory[i])
+        if (get_two_tier_lookup_value(item_qual_locations, item_name, item_quality))
         then
-          -- find target index
-          local target_index = swap_filter_data.start_index + swap_filter_data.filters_set
-          swap_filter_data.filters_set = swap_filter_data.filters_set + 1
-
-          inventory.set_filter(target_index, { name = item_stack.name, quality = item_quality })
-          inventory_slots_already_swapped_into[target_index] = true
-
-          -- Handles the swap itself, if needed.
-          if (target_index == i)
-          then
-            -- ???
-          else
-            -- Prevents errors if trying to swap target/source item into previously-filtered slots.
-            inventory.set_filter(i, nil)
-            inventory[i].swap_stack(inventory[target_index])
-
-            -- will need to check this slot again, something new is here.
-            i = i - 1
-          end
+          table.insert(item_qual_locations[item_name][item_quality], i)
         end
-      else
-        if (i < hard_stop_index)
-        then
-          player.print("skipped" ..
-            i .. "|| swapped: " .. tostring(swapped_here) .. ", readValid: " .. tostring(read_valid))
+      end
+    end
+
+    -- Now that data is constructed, iterate through the filters that need to be set.
+    -- > just grab the first appropriate item from it's recorded place when doing so.
+    local next_filter_index_to_set = 1
+    for name, qual_stacks in pairs(inventory_items_qual_stacks) do
+      for qual, stacks in pairs(qual_stacks) do
+        local filter = { name = name, quality = qual }
+        for i = 1, stacks do
+          -- just shunt it out first? and then check after if the thing you moved was tracked elsewhere
+          local swap_from = table.remove(item_qual_locations[name][qual])
+          inventory.set_filter(next_filter_index_to_set, filter)
+          inventory[swap_from].swap_stack(inventory[next_filter_index_to_set])
+
+          -- now check if you need to keep track of the thing you moved
+          if (inventory[swap_from].valid_for_read)
+          then
+            local swapped_name = inventory[swap_from].name
+            local swapped_qual = get_quality(inventory[swap_from])
+            local array = get_two_tier_lookup_value(item_qual_locations, swapped_name, swapped_qual)
+            if (array)
+            then
+              for _, index in ipairs(array) do
+                if (index == next_filter_index_to_set)
+                then
+                  array[i] = swap_from
+                end
+              end
+            end
+          end
+
+          -- now safe to increment to next filter index
+          next_filter_index_to_set = next_filter_index_to_set + 1
         end
       end
     end
@@ -300,10 +249,4 @@ script.on_event(defines.events.on_gui_opened, function(event)
 
   -- And apply normal sort to clean up everything else.
   inventory.sort_and_merge()
-
-  -- ================================   -- ================================
-
-  -- Testing needed: item in hand slot? what happens when assigning there? swapping there?
-
-  -- ================================   -- ================================
 end)
